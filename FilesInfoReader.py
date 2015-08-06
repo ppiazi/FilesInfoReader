8d9c49a39e81879e07f507336d549d1d55a0202b
Joohyun Lee <ppiazi@gmail.com>
2015-08-06 09:57:27 +0900

    기능 추가  - Hash계산  - modificationo date 가져오기  - file size 가져오기

--- FilesInfoReader.py
+++ FilesInfoReader.py
@@ -1,11 +1,12 @@
 import os
+import time
 import hashlib
 
 __author__ = 'ppiazi'
 
 class FilesInfoReader:
     def __init__(self):
-        self.file_info_dict = {}
+        self.file_info_list = []
         self.FlagModifiedDate = True
         self.FlagCheckSum = True
 
@@ -16,22 +17,46 @@ class FilesInfoReader:
         for (root, dirs, files) in os.walk(self.rootPath):
             print("Entering %s " % (root))
 
-            for file in files:
-                print("\tReading file info : %s ... " % file, end=" ")
-                full_file_name = os.path.join(root, file)
-                f = open(full_file_name, "rb")
-                check_sum = hashfile(f, hashlib.sha256())
+            for afile in files:
+                print("\tReading file info : %s ... " % afile, end=" ")
+                full_file_name = os.path.join(root, afile)
+                check_sum = self.getFileCheckSum(full_file_name)
+                modified_time = self.getFileMTime(full_file_name)
+                file_size = self.getFileSize(full_file_name)
                 print("done")
 
-                self.file_info_dict[full_file_name] = {}
-                self.file_info_dict[full_file_name]["CheckSum"] = check_sum
+                afile_info = {}
+                afile_info["Name"] = full_file_name
+                afile_info["CheckSum"] = check_sum
+                afile_info["MTime"] = modified_time
+                afile_info["Size"] = file_size
+
+                self.file_info_list.append(afile_info)
+
+    def getFileCheckSum(self, file_name):
+        f = open(file_name, "rb")
+        check_sum = hashfile(f, hashlib.sha256())
+        f.close()
+        return check_sum
+
+    def getFileMTime(self, file_name):
+        mtime = os.path.getmtime(file_name)
+        return mtime
+
+    def getFileSize(self, file_name):
+        statinfo = os.stat(file_name)
+        return statinfo.st_size
+
+    def printAll(self):
+        for afile_info in self.file_info_list:
+            print("%s %s %s %d" % (afile_info["Name"], afile_info["CheckSum"], time.ctime(afile_info["MTime"]), afile_info["Size"]))
 
 def hashfile(afile, hasher, blocksize=65535):
     buf = afile.read(blocksize)
     while len(buf) > 0:
         hasher.update(buf)
         buf = afile.read(blocksize)
-    return hasher.digest()
+    return hasher.hexdigest()
 
 if __name__ == "__main__":
     fig = FilesInfoReader()
@@ -39,3 +64,5 @@ if __name__ == "__main__":
     fig.setRootPath("D:\\temp")
 
     fig.iterate()
+
+    fig.printAll()
