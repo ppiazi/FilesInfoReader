7b36590e6c59dae50dc3d74e0b8712ad032b4aa8
Joohyun Lee <ppiazi@gmail.com>
2015-11-01 20:21:12 +0900

    FileInfo 객체를 사용하도록 소스코드 수정. FileInfo 객체를 통해 원하는 정보를 얻을 수 있다.

---
+++ FileInfo.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2015 Joohyun Lee(ppiazi@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+import time
+import binascii
+
+__author__ = 'ppiazi'
+
+SOURCE_CODE_EXT = [".c", "cpp", ".h", ".hpp", ".py", ".cs", ".java"]
+
+class FileInfo:
+    def __init__(self, full_file):
+        self.full_file = full_file
+        self.file_info = {}
+        self.file_info["FilePath"] = self.full_file
+        self.file_info["CheckSum"] = ""
+        self.file_info["FileMTime"] = 0
+        self.file_info["FileSize"] = 0
+        self.file_info["LineCount"] = 0
+        self.file_handler = None
+
+    def readInfo(self):
+        self.file_handler = open(self.full_file, "rb")
+
+        self.readFileCheckSum()
+        self.readFileMTime()
+        self.readFileSize()
+        self.readLineCount()
+
+        self.file_handler.close()
+
+    def getFileCheckSum(self):
+        return self.file_info["CheckSum"]
+
+    def getFileMTime(self):
+        return self.file_info["FileMTime"]
+
+    def getFileSize(self):
+        return self.file_info["FileSize"]
+
+    def getFileLineCount(self):
+        return self.file_info["LineCount"]
+
+    def readFileCheckSum(self):
+        """
+        파일의 CRC32를 이용한 체크섬을 계산한다.
+        binascii 모듈을 사용한다.
+
+        :param file_name: 파일 이름
+        :return: CRC32 결과
+        """
+        # reset file pointer
+        self.file_handler.seek(0)
+        file_data = self.file_handler.read()
+        check_sum_int = binascii.crc32(file_data) & 0xFFFFFFFF
+        check_sum_hex = hex(check_sum_int).upper()
+
+        self.file_info["CheckSum"] = check_sum_hex
+
+    def readFileMTime(self):
+        """
+        파일의 수정된 날짜 및 시간을 반환한다.
+        os.path.getmtime 을 사용한다.
+
+        :param file_name:
+        :return:
+        """
+        mtime = os.path.getmtime(self.full_file)
+        t = time.localtime(mtime)
+        mtime_str = time.strftime("%Y-%m-%d %H:%M:%S", t)
+
+        self.file_info["FileMTime"] = mtime_str
+
+    def readFileSize(self):
+        """
+        파일의 크기를 반환한다.
+        os.stat 를 사용한다.
+
+        :param file_name:
+        :return:
+        """
+        statinfo = os.stat(self.full_file)
+
+        self.file_info["FileSize"] = statinfo.st_size
+
+    def readLineCount(self):
+        """
+        C/C++/C#/Java 등 소스코드의 라인수를 반환한다.
+
+        :param file_name:
+        :return:
+        """
+        ext = os.path.splitext(self.full_file)[-1]
+        line_count = 0
+
+        if ext.lower() in SOURCE_CODE_EXT:
+            # reset file pointer
+            self.file_handler.seek(0)
+            file_lines = self.file_handler.readlines()
+
+            line_count = len(file_lines)
+
+        self.file_info["LineCount"] = line_count
--- FilesInfoReader.py
+++ FilesInfoReader.py
@@ -15,15 +15,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import os
-import time
-import binascii
 import logging
 import logging.handlers
 import pandas as pd
+import FileInfo
 
 __author__ = 'ppiazi'
-
-SOURCE_CODE_EXT = [".c", "cpp", ".h", ".hpp", ".py", ".cs", ".java"]
 
 class FilesInfoReader:
     def __init__(self):
@@ -45,72 +42,17 @@ class FilesInfoReader:
 
             for afile in files:
                 self.logger.info("\tReading file info : %s" % afile)
-
                 full_file_name = os.path.join(root, afile)
-                check_sum = self.getFileCheckSum(full_file_name)
-                modified_time_str = self.getFileMTime(full_file_name)
-                file_size = self.getFileSize(full_file_name)
-                source_code_line_count = self.getLineCount(full_file_name)
+
+                file_info = FileInfo.FileInfo(full_file_name)
+                file_info.readInfo()
+
+                check_sum = file_info.getFileCheckSum()
+                modified_time_str = file_info.getFileMTime()
+                file_size = file_info.getFileSize()
+                source_code_line_count = file_info.getFileLineCount()
 
                 self.file_info_list.loc[full_file_name] = [check_sum, modified_time_str, file_size, source_code_line_count]
-
-    def getFileCheckSum(self, file_name):
-        """
-        파일의 CRC32를 이용한 체크섬을 계산한다.
-        binascii 모듈을 사용한다.
-
-        :param file_name: 파일 이름
-        :return: CRC32 결과
-        """
-        f = open(file_name, "rb")
-        file_data = f.read()
-        check_sum_int = binascii.crc32(file_data) & 0xFFFFFFFF
-        check_sum_hex = hex(check_sum_int).upper()
-        f.close()
-        return check_sum_hex
-
-    def getFileMTime(self, file_name):
-        """
-        파일의 수정된 날짜 및 시간을 반환한다.
-        os.path.getmtime 을 사용한다.
-
-        :param file_name:
-        :return:
-        """
-        mtime = os.path.getmtime(file_name)
-        t = time.localtime(mtime)
-        mtime_str = time.strftime("%Y-%m-%d %H:%M:%S", t)
-        return mtime_str
-
-    def getFileSize(self, file_name):
-        """
-        파일의 크기를 반환한다.
-        os.stat 를 사용한다.
-
-        :param file_name:
-        :return:
-        """
-        statinfo = os.stat(file_name)
-        return statinfo.st_size
-
-    def getLineCount(self, file_name):
-        """
-        C/C++/C#/Java 등 소스코드의 라인수를 반환한다.
-
-        :param file_name:
-        :return:
-        """
-        ext = os.path.splitext(file_name)[-1]
-        line_count = 0
-
-        if ext.lower() in SOURCE_CODE_EXT:
-            f = open(file_name, "rb")
-            file_lines = f.readlines()
-            f.close()
-
-            line_count = len(file_lines)
-
-        return line_count
 
     def printAll(self):
         for index, afile_info in self.file_info_list.iterrows():
