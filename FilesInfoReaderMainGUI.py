df3a06b47658a3dd8cb0b33629c7429c3451f1f4
Joohyun Lee <ppiazi@gmail.com>
2016-01-23 17:46:19 +0900

    - Hash 방법 선택 가능하도록 기능 추가 - output 파일 지정할 수 있도록 기능 추가

--- FilesInfoReaderMainGUI.py
+++ FilesInfoReaderMainGUI.py
@@ -16,36 +16,83 @@ limitations under the License.
 """
 
 import sys
-import time
+import os
 import qt4.MainDlg
 from PyQt4 import QtCore, QtGui
 import FileInfo
+import FilesInfoReader
 
 class FilesInfoReaderMainGUI(QtGui.QDialog, qt4.MainDlg.Ui_Dialog):
     def __init__(self, parent=None):
         super(FilesInfoReaderMainGUI, self).__init__(parent)
         self.setupUi(self)
-        self.BtnBrowse.clicked.connect(self.showFileBrowserDlg)
+        self.BtnBrowse.clicked.connect(self.showTargetFolderDlg)
+        self.BtnOutputBrowse.clicked.connect(self.showOutputFileDlg)
         self.BtnStart.clicked.connect(self.readInfo)
         self.BtnExit.clicked.connect(QtCore.QCoreApplication.instance().quit)
         self.ChkBoxExtOnly.stateChanged.connect(self.changeExtOnly)
         self.EditExtList.setText(",".join(FileInfo.SOURCE_CODE_EXT))
+        self.RadBtnHashGroup = QtGui.QButtonGroup()
+        self.RadBtnHashGroup.addButton(self.RadBtnCrc32)
+        self.RadBtnHashGroup.addButton(self.RadBtnMD5)
+        self.RadBtnHashGroup.addButton(self.RadBtnSHA1)
+        self._extonly_flag = False
 
     def changeExtOnly(self, state):
         if state == QtCore.Qt.Checked:
             self.EditExtList.setEnabled(True)
+            self._extonly_flag = True
         else:
             self.EditExtList.setEnabled(False)
+            self._extonly_flag = False
 
-    def showFileBrowserDlg(self):
+    def showTargetFolderDlg(self):
         folder = QtGui.QFileDialog.getExistingDirectory(self, "Select Folder")
-        self.TargetFolder = folder
         self.EditTargetFolder.setText(folder)
+
+    def showOutputFileDlg(self):
+        output_file = QtGui.QFileDialog.getSaveFileName(self, "Output File")
+        self.EditOutput.setText(output_file)
 
     def readInfo(self):
         # check target folder validation
+        target_folder = self.EditTargetFolder.text()
+        if os.path.exists(target_folder) == False:
+            self._warning("Invalid target folder")
+            return
+        # check source code only option
+        source_code_only = False
+        if self._extonly_flag == True:
+            source_code_only = True
+            try:
+                ext_list = self.EditExtList.text().split(",")
+            except:
+                self._warning("Invalid ext list")
+                return
+            FileInfo.SOURCE_CODE_EXT = ext_list
 
-        # 
+        # check hash method
+        if self.RadBtnCrc32.isChecked() == True:
+            hash_method = "crc32"
+        elif self.RadBtnMD5.isChecked() == True:
+            hash_method = "md5"
+        elif self.RadBtnSHA1.isChecked() == True:
+            hash_method = "sha1"
+        else:
+            hash_method = "crc32"
+
+        fir = FilesInfoReader.FilesInfoReader(hash_method)
+        fir.setRootPath(target_folder)
+        fir.iterate(source_code_only)
+        fir.saveAsCsv(self.EditOutput.text())
+        self._info("Done")
+
+
+    def _warning(self, msg):
+        QtGui.QMessageBox.question(self, "Warning", msg, QtGui.QMessageBox.Yes)
+
+    def _info(self, msg):
+        QtGui.QMessageBox.question(self, "Info", msg, QtGui.QMessageBox.Yes)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
