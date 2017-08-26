f47ebdebe53149e964343b6cd4b1e9c3673de3ab
ppiazi <ppiazi@gmail.com>
2017-08-26 12:04:44 +0900

    #19 Add ClocXmlParser and test file/data

---
+++ com/tistory/ppiazi/fir/ClocXmlParser.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2015 Joohyun Lee(ppiazi@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from bs4 import BeautifulSoup
+
+class ClocXmlParser:
+    def __init__(self):
+        pass
+
+    def openXml(self, xml_file):
+        """
+        open xml_file and make beautifulsoup object with it
+
+        :return:
+        """
+        try:
+            f = open(xml_file, "r")
+        except FileNotFoundError as e:
+            print(str(e))
+            return None
+
+        self.soup = BeautifulSoup(f, "xml")
+
+        f.close()
+
+        return self.soup
+
+    def parseXml(self, xml_file):
+        """
+        cloc xml 파일을 읽어 dict 형태로 반환한다.
+
+        :return:
+        """
+        soup = self.openXml(xml_file)
+
+        file_objects = soup.findAll("file")
+
+        file_line_info = {}
+
+        try:
+            for file_object in file_objects:
+                fli = {}
+                fli["blank"] = int(file_object["blank"])
+                fli["comment"] = int(file_object["comment"])
+                fli["code"] = int(file_object["code"])
+                fli["lang"] = file_object["language"]
+
+                file_line_info[file_object["name"]] = fli
+        except Exception as e:
+            print(str(e))
+            file_line_info = None
+
+        return file_line_info
---
+++ com/tistory/ppiazi/fir/test/ClocXmlParserTest.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2015 Joohyun Lee(ppiazi@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from com.tistory.ppiazi.fir.ClocXmlParser import ClocXmlParser
+import unittest
+
+TEST_TARGET_NOFILE = "nofile.xml"
+TEST_TARGET1 = "test1.xml"
+TEST_TARGET2 = "test2.xml"
+
+class ClocXmlParserTest(unittest.TestCase):
+    testTarget = None
+
+    def setUp(self):
+        self.testTarget = ClocXmlParser()
+
+    def tearDown(self):
+        pass
+
+    def test_parseXml_with_data1(self):
+        soup = self.testTarget.openXml(TEST_TARGET1)
+        assert( soup != None )
+
+    def test_parseXml_with_nofile(self):
+        soup = self.testTarget.openXml(TEST_TARGET_NOFILE)
+        assert ( soup == None )
+
+    def test_parseXml_normal_data(self):
+        file_line_info = self.testTarget.parseXml(TEST_TARGET1)
+        assert(len(file_line_info) == 236)
+        assert (file_line_info["cloc-1.72\\Unix\\cloc"]["code"] == 8424)
+        assert (file_line_info[r"cloc-1.72\tests\inputs\issues\114\bar\bee\inner_most.js"]["blank"] == 0)
+        assert (file_line_info[r"cloc-1.72\tests\inputs\issues\132\C-Ansi.c"]["code"] == 7)
+
+    def test_parseXml_normal_data_with_korean_path(self):
+        file_line_info = self.testTarget.parseXml(TEST_TARGET2)
+        assert (len(file_line_info) == 73)
+        assert (file_line_info[r".\새 폴더 (2)\result2.xml"]["code"] == 17)
+        assert (file_line_info[r".\새 폴더\호호\한글2.c"]["blank"] == 2)
+
+if __name__ == "__main__":
+    unittest.main()
