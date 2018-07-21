94fb4015368b6e89fade5af2f8a58cb6a56eb65d
Joohyun Lee <ppiazi@gmail.com>
2018-07-21 20:49:16 +0900

    Modify UI freezing symptom #22
    - Seperate worker thread from UI thread
    - Enable / disable UI components while waiting

--- FilesInfoReaderMainGUI.py
+++ FilesInfoReaderMainGUI.py
@@ -28,6 +28,28 @@ import qt4.MainDlg
 
 CONFIG_FILE = "config.ini"
 
+class FirWorksDoneSignal(QtCore.QObject):
+    sig = QtCore.Signal(str)
+
+class FirWorker(QtCore.QThread):
+    def __init__(self, parent=None):
+        QtCore.QThread.__init__(self, parent)
+        self.running = False
+        self.signal = FirWorksDoneSignal()
+
+    def set_data(self, fir_ins, ext_only, igr_enabled, line_info = None):
+        self.fir_ins = fir_ins
+        self.ext_only = ext_only
+        self.igr_enabled = igr_enabled
+        self.line_info = line_info
+
+    def run(self):
+        try:
+            self.fir_ins.iterate(self.ext_only, self.igr_enabled, self.line_info)
+            self.signal.sig.emit("DONE")
+        except Exception as e:
+            self.signal.sig.emit(str(e))
+
 class FilesInfoReaderMainGUI(QtWidgets.QDialog, qt4.MainDlg.Ui_Dialog):
     """
     GUI 버전 FilesInfoReader
@@ -36,7 +58,11 @@ class FilesInfoReaderMainGUI(QtWidgets.QDialog, qt4.MainDlg.Ui_Dialog):
         super(FilesInfoReaderMainGUI, self).__init__(parent)
         self.setupUi(self)
 
+        # 설정 파일을 읽는다.
         self._load_config()
+
+        # UI 전체 enable / disable을 위한 container를 생성한다.
+        self._ui_handles = {}
 
         self.BtnBrowse.clicked.connect(self.show_target_folder_dlg)
         self.BtnOutputBrowse.clicked.connect(self.show_output_file_dlg)
@@ -44,6 +70,13 @@ class FilesInfoReaderMainGUI(QtWidgets.QDialog, qt4.MainDlg.Ui_Dialog):
         self.BtnStart.clicked.connect(self.read_info)
         self.BtnExit.clicked.connect(self.close_app)
         self.ChkBoxExtOnly.stateChanged.connect(self.change_ext_only)
+
+        self._ui_handles["BtnBrowse"] = self.BtnBrowse
+        self._ui_handles["BtnOutputBrowse"] = self.BtnOutputBrowse
+        self._ui_handles["BtnClocPathBrowse"] = self.BtnClocPathBrowse
+        self._ui_handles["BtnStart"] = self.BtnStart
+        self._ui_handles["BtnExit"] = self.BtnExit
+        self._ui_handles["ChkBoxExtOnly"] = self.ChkBoxExtOnly
 
         self.EditSourceExtList.setText(self._config.get("SETTING", "source_ext_list", fallback=",".join(
             FileInfo.SOURCE_CODE_EXT)))
@@ -54,6 +87,13 @@ class FilesInfoReaderMainGUI(QtWidgets.QDialog, qt4.MainDlg.Ui_Dialog):
         self.EditTargetFolder.setText(self._config.get("SETTING", "target_folder", fallback=""))
         self.EditClocPath.setText(self._config.get("SETTING", "cloc_path", fallback=""))
 
+        self._ui_handles["EditSourceExtList"]  = self.EditSourceExtList
+        #self._ui_handles["EditExtList"]  = self.EditExtList
+        self._ui_handles["EditOutput"]  = self.EditOutput
+        #self._ui_handles["EditIgnorePattern"]  = self.EditIgnorePattern
+        self._ui_handles["EditTargetFolder"]  = self.EditTargetFolder
+        #self._ui_handles["EditClocPath"]  = self.EditClocPath
+
         self.ChkBoxIgnore.stateChanged.connect(self.change_ignore)
         self.ChkBoxClocUse.stateChanged.connect(self.change_cloc_use)
         self.RadBtnHashGroup = QtWidgets.QButtonGroup()
@@ -62,10 +102,19 @@ class FilesInfoReaderMainGUI(QtWidgets.QDialog, qt4.MainDlg.Ui_Dialog):
         self.RadBtnHashGroup.addButton(self.RadBtnSHA1)
         self.setWindowTitle("FilesInfoReader - %s by ppiazi" % (FilesInfoReaderMain.__version__))
 
+        self._ui_handles["ChkBoxIgnore"] = self.ChkBoxIgnore
+        self._ui_handles["ChkBoxClocUse"] = self.ChkBoxClocUse
+        self._ui_handles["RadBtnCrc32"] = self.RadBtnCrc32
+        self._ui_handles["RadBtnMD5"] = self.RadBtnMD5
+        self._ui_handles["RadBtnSHA1"] = self.RadBtnSHA1
+
         self._extonly_flag = False
         self._igr_enabled_flag = False
         self._cloc_use_flag = False
 
+        self._worker = FirWorker()
+        self._worker.signal.sig.connect(self._check_worker)
+        
         self.setFixedSize(self.size())
 
     def close_app(self):
@@ -100,9 +149,11 @@ class FilesInfoReaderMainGUI(QtWidgets.QDialog, qt4.MainDlg.Ui_Dialog):
         :return:
         """
         if state == QtCore.Qt.Checked:
+            self._ui_handles["EditIgnorePattern"] = self.EditIgnorePattern
             self.EditIgnorePattern.setEnabled(True)
             self._igr_enabled_flag = True
         else:
+            self._ui_handles["EditIgnorePattern"] = None
             self.EditIgnorePattern.setEnabled(False)
             self._igr_enabled_flag = False
 
@@ -113,10 +164,12 @@ class FilesInfoReaderMainGUI(QtWidgets.QDialog, qt4.MainDlg.Ui_Dialog):
         :return:
         """
         if state == QtCore.Qt.Checked:
+            self._ui_handles["BtnClocPathBrowse"] = self.BtnClocPathBrowse
             self.BtnClocPathBrowse.setEnabled(True)
             #self.EditClocPath.setEnabled(True)
             self._cloc_use_flag = True
         else:
+            self._ui_handles["BtnClocPathBrowse"] = None
             self.BtnClocPathBrowse.setEnabled(False)
             #self.EditClocPath.setEnabled(False)
             self._cloc_use_flag = False
@@ -128,9 +181,11 @@ class FilesInfoReaderMainGUI(QtWidgets.QDialog, qt4.MainDlg.Ui_Dialog):
         :return:
         """
         if state == QtCore.Qt.Checked:
+            self._ui_handles["EditExtList"] = self.EditExtList
             self.EditExtList.setEnabled(True)
             self._extonly_flag = True
         else:
+            self._ui_handles["EditExtList"] = None
             self.EditExtList.setEnabled(False)
             self._extonly_flag = False
 
@@ -200,24 +255,44 @@ class FilesInfoReaderMainGUI(QtWidgets.QDialog, qt4.MainDlg.Ui_Dialog):
         else:
             hash_method = "crc32"
 
-        fir = FilesInfoReader.FilesInfoReader(hash_method, self._cloc_use_flag)
-        fir.set_root_path(target_folder)
+        self.fir = FilesInfoReader.FilesInfoReader(hash_method, self._cloc_use_flag)
+
+        self.fir.set_root_path(target_folder)
         if igr_enabled == True:
-            fir.set_ignore_pattern(self.EditIgnorePattern.text())
+            self.fir.set_ignore_pattern(self.EditIgnorePattern.text())
 
         if self._cloc_use_flag == False:
-            fir.iterate(ext_only, igr_enabled)
+            line_info = None
         else:
             p_use_cloc_path = self.EditClocPath.text()
-            line_info = fir.start_cloc(p_use_cloc_path)
-            fir.iterate(ext_only, igr_enabled, line_info)
+            line_info = self.fir.start_cloc(p_use_cloc_path)
 
-        try:
-            fir.save(self.EditOutput.text())
-        except Exception as e:
-            self._warning("Error to save output : " + str(e))
+        #start fir.iterate
+        self._worker.set_data(self.fir, ext_only, igr_enabled, line_info)
+        self._all_disable()
+        self._worker.start()   
+
+    def _all_disable(self):
+        for v in self._ui_handles.values():
+            if v != None:
+                v.setEnabled(False)
+
+    def _all_enable(self):
+        for v in self._ui_handles.values():
+            if v != None:
+                v.setEnabled(True)
+
+    def _check_worker(self, end_status):
+        if end_status == "DONE":
+            try:
+                self.fir.save(self.EditOutput.text())
+            except Exception as e:
+                self._warning("Error to save output : " + str(e))
+            else:
+                self._info("Done")
         else:
-            self._info("Done")
+            self._warning("Error to iterate files : " + end_status)
+        self._all_enable()
 
     def _warning(self, msg):
         """
